a6b4a3159c2a7696eca4da7329f0819ac9995e89
Asakura Mizu <asakuramizu111@gmail.com>
2025-07-07 23:00:04 +0800

    adapt for oscomp
    Co-authored-by: mivik <mivikq@gmail.com>

--- build.rs
+++ build.rs
@@ -1,80 +1,53 @@
+use std::env;
 use std::path::{Path, PathBuf};
 use std::process::Command;
-use std::{env, fs};
 
 fn main() {
     let c_path = PathBuf::from("c/lwext4")
         .canonicalize()
         .expect("cannot canonicalize path");
 
-    let lwext4_make = Path::new("c/lwext4/toolchain/musl-generic.cmake");
-    let lwext4_patch = Path::new("c/lwext4-make.patch").canonicalize().unwrap();
-
-    if !Path::new(lwext4_make).exists() {
-        println!("Retrieve lwext4 source code");
-        let git_status = Command::new("git")
-            .args(&["submodule", "update", "--init", "--recursive"])
-            .status()
-            .expect("failed to execute process: git submodule");
-        assert!(git_status.success());
-
-        println!("To patch lwext4 src");
-        Command::new("git")
-            .args(&["apply", lwext4_patch.to_str().unwrap()])
-            .current_dir(c_path.clone())
-            .spawn()
-            .expect("failed to execute process: git apply patch");
-
-        fs::copy(
-            "c/musl-generic.cmake",
-            "c/lwext4/toolchain/musl-generic.cmake",
-        )
-        .unwrap();
-    }
-
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    let lwext4_lib = &format!("lwext4-{}", arch);
-    let lwext4_lib_path = &format!("c/lwext4/lib{}.a", lwext4_lib);
-    if !Path::new(lwext4_lib_path).exists() {
-        let status = Command::new("make")
-            .args(&[
+    let lwext4_lib = &format!("lwext4-{arch}");
+    {
+        let mut command = Command::new("make");
+        command
+            .args([
                 "musl-generic",
                 "-C",
                 c_path.to_str().expect("invalid path of lwext4"),
             ])
-            .arg(&format!("ARCH={}", arch))
+            .arg(format!(
+                "ULIBC={}",
+                if cfg!(feature = "std") { "OFF" } else { "ON" }
+            ));
+        match env::var("TARGET").unwrap().as_str() {
+            "aarch64-unknown-none-softfloat" => {
+                command.arg("CFLAGS=-mgeneral-regs-only");
+            }
+            "loongarch64-unknown-none-softfloat" => {
+                command.arg("CFLAGS=-msoft-float");
+            }
+            _ => {}
+        }
+        let status = command
             .status()
             .expect("failed to execute process: make lwext4");
         assert!(status.success());
-
-        if !Path::new("src/bindings.rs").exists() {
-            let cc = &format!("{}-linux-musl-gcc", arch);
-            let output = Command::new(cc)
-                .args(["-print-sysroot"])
-                .output()
-                .expect("failed to execute process: gcc -print-sysroot");
-
-            let sysroot = core::str::from_utf8(&output.stdout).unwrap();
-            let sysroot = sysroot.trim_end();
-            let sysroot_inc = &format!("-I{}/include/", sysroot);
-
-            generates_bindings_to_rust(sysroot_inc);
-        }
     }
+    {
+        let cc = &format!("{arch}-linux-musl-gcc");
+        let output = Command::new(cc)
+            .args(["-print-sysroot"])
+            .output()
+            .expect("failed to execute process: gcc -print-sysroot");
 
-    /* No longer need to implement the libc.a
-    let libc_name = &format!("c-{}", arch);
-    let libc_dir = env::var("LIBC_BUILD_TARGET_DIR").unwrap_or(String::from("./"));
-    let libc_dir = PathBuf::from(libc_dir)
-        .canonicalize()
-        .expect("cannot canonicalize LIBC_BUILD_TARGET_DIR");
+        let sysroot = core::str::from_utf8(&output.stdout).unwrap();
+        let sysroot = sysroot.trim_end();
+        let sysroot_inc = &format!("-I{sysroot}/include/");
 
-    println!("cargo:rustc-link-lib=static={libc_name}");
-    println!(
-        "cargo:rustc-link-search=native={}",
-        libc_dir.to_str().unwrap()
-    );
-    */
+        generates_bindings_to_rust(sysroot_inc);
+    }
 
     println!("cargo:rustc-link-lib=static={lwext4_lib}");
     println!(
@@ -82,12 +55,19 @@ fn main() {
         c_path.to_str().unwrap()
     );
     println!("cargo:rerun-if-changed=c/wrapper.h");
-    println!("cargo:rerun-if-changed={}", c_path.to_str().unwrap());
+    println!("cargo:rerun-if-changed={}/src", c_path.to_str().unwrap());
 }
 
 fn generates_bindings_to_rust(mpath: &str) {
+    let target = env::var("TARGET").unwrap();
+    if target.ends_with("-softfloat") {
+        // Clang does not recognize the `-softfloat` suffix
+        unsafe { env::set_var("TARGET", target.replace("-softfloat", "")) };
+    }
+
     let bindings = bindgen::Builder::default()
         .use_core()
+        .wrap_unsafe_ops(true)
         // The input header we would like to generate bindings for.
         .header("c/wrapper.h")
         //.clang_arg("--sysroot=/path/to/sysroot")
@@ -97,14 +77,39 @@ fn generates_bindings_to_rust(mpath: &str) {
         .clang_arg("-I./c/lwext4/build_musl-generic/include/")
         .layout_tests(false)
         // Tell cargo to invalidate the built crate whenever any of the included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .parse_callbacks(Box::new(CustomCargoCallbacks))
         // Finish the builder and generate the bindings.
         .generate()
         .expect("Unable to generate bindings");
 
+    // Restore the original target environment variable
+    unsafe { env::set_var("TARGET", target) };
+
     // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from("src");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+#[derive(Debug)]
+struct CustomCargoCallbacks;
+impl bindgen::callbacks::ParseCallbacks for CustomCargoCallbacks {
+    fn header_file(&self, filename: &str) {
+        add_include(filename);
+    }
+
+    fn include_file(&self, filename: &str) {
+        add_include(filename);
+    }
+
+    fn read_env_var(&self, key: &str) {
+        println!("cargo:rerun-if-env-changed={key}");
+    }
+}
+
+fn add_include(filename: &str) {
+    if !Path::new(filename).ends_with("ext4_config.h") {
+        println!("cargo:rerun-if-changed={filename}");
+    }
 }
